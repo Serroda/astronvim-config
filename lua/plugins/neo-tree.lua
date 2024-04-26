c8f155fa03eaed56310244987fb41895d2c68960
Serroda <jesuscarclem30@gmail.com>
2024-04-26 19:08:27 +0200

    feat(neo-tree):Show hidden files

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,6 +2,11 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
       window = {
         position = "right",
       },
