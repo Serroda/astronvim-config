0ad751260480727c409ccda93043950752249267
Serroda <jesuscarclem30@gmail.com>
2024-06-28 21:23:30 +0200

    add: markview.nvim

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,7 +1,17 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && npm install && git restore .",
-  init = function() vim.g.mkdp_filetypes = { "markdown" } end,
-  ft = { "markdown" },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install && git restore .",
+    init = function() vim.g.mkdp_filetypes = { "markdown" } end,
+    ft = { "markdown" },
+  },
+  {
+    "OXY2DEV/markview.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons", -- Used by the code bloxks
+    },
+
+    config = function() require("markview").setup() end,
+  },
 }
