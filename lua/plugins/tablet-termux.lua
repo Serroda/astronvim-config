70e81cb952ff717cff3f99458f291e75f49e7b8c
Serroda <jesuscarclem30@gmail.com>
2024-07-31 10:17:48 +0200

    Termux config
    Install lua-language-server ans stylua

---
+++ lua/plugins/tablet-termux.lua
@@ -0,0 +1,18 @@
+-- INSTALL LUA_LANGUAGE_SERVER AND STYLUA FOR TERMU
+
+return {
+  { "AstroNvim/astrolsp", opts = {
+    servers = {
+      "lua_ls",
+    },
+  } },
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local null_ls = require "null-ls"
+      opts.sources = require("astrocore").list_insert_unique(opts.sources, {
+        null_ls.builtins.formatting.stylua,
+      })
+    end,
+  },
+}
