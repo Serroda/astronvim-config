a169f289348ec7942f2fd7173a26f325cdf7af14
Jesus <35766979+Serroda@users.noreply.github.com>
2024-08-27 22:46:40 +0200

    update: remove stylua and lua_ls from ensured_installed

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -8,7 +8,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "lua_ls",
+        -- "lua_ls",
         "jsonls",
         "html",
         "cssls",
@@ -23,7 +23,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "stylua",
+        -- "stylua",
         -- add more arguments for adding more null-ls sources
       })
     end,
