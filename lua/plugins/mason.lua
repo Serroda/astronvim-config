cdbc0866c23f595e517521912b355ff5f6339e3a
serroda <jesuscarclem30@gmail.com>
2025-03-27 07:28:29 +0100

    Update to v5

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,19 +1,17 @@
 -- Customize Mason plugins
 ---@type LazySpec
 return {
-  -- use mason-lspconfig to configure LSP installations
+  -- use mason-tool-installer for automatically installing Mason packages
   {
-    "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "jsonls",
-        "html",
-        "cssls",
-        "unocss",
-        -- add more arguments for adding more language servers
-      })
-    end,
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    -- overrides `require("mason-tool-installer").setup(...)`
+    opts = {
+      ensure_installed = {
+        "json-lsp",
+        "html-lsp",
+        "json-lsp",
+        "unocss-language-server",
+      },
+    },
   },
 }
