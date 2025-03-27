277a07d13d20d5692ed8a3490514d06770049c09
serroda <jesuscarclem30@gmail.com>
2025-03-27 11:25:48 +0100

    Revert "Fix typo"
    This reverts commit 3fd0d2f506f92da30a9221dc057c14d8581b87c9.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,15 +3,30 @@
 return {
   -- use mason-lspconfig to configure LSP installations
   {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    -- overrides `require("mason-tool-installer").setup(...)`
-    opts = {
-      ensure_installed = {
-        "json-lsp",
-        "html-lsp",
-        "css-lsp",
-        "unocss-language-server",
-      },
-    },
+    "williamboman/mason-lspconfig.nvim",
+    -- overrides `require("mason-lspconfig").setup(...)`
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "lua_ls",
+        "jsonls",
+        "html",
+        "cssls",
+        "unocss",
+        -- add more arguments for adding more language servers
+      })
+    end,
+  },
+  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+  {
+    "jay-babu/mason-null-ls.nvim",
+    -- overrides `require("mason-null-ls").setup(...)`
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "stylua",
+        -- add more arguments for adding more null-ls sources
+      })
+    end,
   },
 }
